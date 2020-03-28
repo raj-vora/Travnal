fcce3d22015ed1ad1734658cc3336c96e6e83789
raj-vora <raj.jv@somaiya.edu>
2020-03-28 19:30:12 +0530

    backend user registration and login

---
+++ travnal-backend/models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+var uniqueValidator = require('mongoose-unique-validator')
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 3
+    },
+    passwordHash: {
+        type: String,
+        required: true
+    },
+})
+
+userSchema.plugin(uniqueValidator)
+
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+            // the passwordHash should not be revealed
+        delete returnedObject.passwordHash
+    }
+})
+
+module.exports = mongoose.model('User', userSchema)
